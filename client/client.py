be99d3cc39d6bcba393e640c4f48e57253e6ef19
Nikos Mouzakitis <nikos@virtualopensystems.com>
2020-06-08 06:11:03 +0200

    running iterational for 1 client

---
+++ client/client.py
@@ -0,0 +1,56 @@
+import socket
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal, misc
+from scipy.fftpack import fft, ifft, fftfreq
+
+SAMPLES = 50
+# data domain
+t1 = np.zeros([SAMPLES,1])
+#init time domain
+tval = 0.000
+
+for i in range(0,SAMPLES):
+	t1[i,0] = tval
+	tval+=0.004
+
+s1 = np.zeros([SAMPLES, 1])
+s = socket.socket()
+host = socket.gethostname()
+port = 58111
+print("Connecting to ECG server")
+s.connect((host, port))
+
+while(True):
+	run = 0
+	print("Received from server:\n")
+	data_batch = s.recv(1024)
+
+	#composing the samples received back into doubles.
+	for i in range(0,SAMPLES):
+		b=b""	
+
+		for k in range(0,8):
+			b += data_batch[8*run+k].to_bytes(1,byteorder='big')
+	
+		s1[run, 0], = struct.unpack('d', b) 
+		#print("Value composed: ", s1[0,run])
+		run += 1
+
+	print(s1)
+	#plot
+	plt.plot(t1, s1)
+	axes = plt.gca()
+	axes.set_ylim([-1,1.2])
+	plt.rcParams['figure.figsize'] = [10 ,4]
+	plt.title('ECG Signal received from Server')
+	plt.xlabel('Time(seconds)')
+	plt.ylabel('ECG(mV)')
+	plt.pause(1)
+	plt.cla()
+plt.show()
+
+
+s.close()
+
---
+++ server/server.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal, misc 
+from scipy.fftpack import fft, ifft, fftfreq
+import socket
+import struct #pack in double to send over network
+import time
+print("ECG server is powered on\n")
+ecg = pd.read_csv("../sample_data/samples.csv")
+print("read sample data\n")
+
+# t is the time dimension
+t = ecg.iloc[:,0]  
+# s is the actual signal value
+signal = ecg.iloc[:,1]   
+dt = t[1] - t[0]
+
+counter_signal = 0
+
+s = socket.socket()
+host = socket.gethostname()
+port = 58111
+
+s.bind((host, port))
+s.listen(5)
+
+con, addr = s.accept()
+print("Connection from: ", addr)
+	
+while(True):
+	print("Sending batch")
+#send the first 50 samples, that define 2 second in the given dataset.	
+	to_send=b""	
+	for i in range(0,50):
+		print("adding sample: ",signal[counter_signal])
+		b = struct.pack('d',signal[counter_signal])
+		to_send+=b
+		counter_signal+=1
+
+	sended=con.send(to_send)
+	time.sleep(1)	# sleep 1 second
+
